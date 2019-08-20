78383035aee059cc6aa3ab796854041d4d5646cc
Mateus <gamevlogs.mateus20@gmail.com>
2019-08-20 04:11:14 -0500

    Some phone things.

--- phone/conectar_bd.php
+++ phone/conectar_bd.php
@@ -2,12 +2,12 @@
     $s="localhost";
     $u="mateus_root";
     $p="12345679";
-    $b="reserv";
+    $b="onlinecongress";
 
-	$conex = new mysqli($s, $u, $p, $b) or die('No se pudo conectar a la Base de Datos');
-	mysqli_set_charset($conex, "utf8");
-	if ($conex->connect_errno) {
-		printf("Conncect failed: %s\n", $conex->connect_error);
-		exit();
-	}
+    $conex = new mysqli($s, $u, $p, $b) or die('No se pudo conectar a la Base de Datos');
+    mysqli_set_charset($conex, "utf8");
+    if ($conex->connect_errno) {
+        printf("Conncect failed: %s\n", $conex->connect_error);
+        exit();
+    }    
 ?>
