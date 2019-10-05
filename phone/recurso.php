b92db73796a036d24b1770da6c597b53d9bec218
Mateus <atrujillo66@itfip.edu.co>
2019-10-05 17:26:03 -0500

    Test resource.

---
+++ phone/recurso.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>Recurso</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+  <!-- Favicons -->
+  <link href="../img-main/favicon.png" rel="icon">
+  <link href="../img-main/apple-touch-icon.png" rel="apple-touch-icon">
+  <style>
+    body {
+        background: #000;
+    }
+    iframe {
+        margin: 0 auto;
+        padding: 0;
+        display: block;
+        position: relative;
+        width: 96vw; 
+        height: 54vw;
+        /* 100/56.25 = 560/315 = 1.778 */
+        background: #000;  
+    }
+    </style>
+</head>
+
+<body>
+<iframe src="https://www.youtube-nocookie.com/embed/tGHoknod5WM?start=142"
+    frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen>
+</iframe>
+</body>
+</html>
