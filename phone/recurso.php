eb9d12bd214562f98e257e3199d322392657b529
Mateus <atrujillo66@itfip.edu.co>
2019-10-13 01:23:48 -0500

    Hotfix: Optimized resource file.

--- phone/recurso.php
+++ phone/recurso.php
@@ -1,30 +1,20 @@
 <!DOCTYPE html>
-<html lang="es">
 <head>
   <meta charset="utf-8">
   <title>Recurso</title>
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <meta content="" name="keywords">
-  <meta content="" name="description">
-  
   <style>
-    body {
-        background: #000;
-    }
+    html, body { background: #000 !important; overflow: hidden !important; }
     iframe {
-        margin: 0 auto;
-        padding: 0;
-        display: block;
-        position: relative;
-        width: 96vw; 
-        height: 54vw;
-        /* 100/56.25 = 560/315 = 1.778 */
-        background: #000;  
+        margin: 0 auto; padding: 0;
+        top: 0; bottom: 0; left: 0; right: 0;
+        display: block !important; position: absolute !important;
+        width: 100vw !important; height: 56.25vw !important;
+        /* 100/56.25 = 560/315 = 1.778 */ background: #000;  
     }
     </style>
 </head>
-
 <body>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/MAlSjtxy5ak" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<?= '<iframe src="https://www.youtube.com/embed/MAlSjtxy5ak" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' ?>
 </body>
 </html>
