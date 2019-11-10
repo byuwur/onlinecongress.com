229c84891e728fd6ec40ad582398dcf0ff54cd78
Mateus <atrujillo66@itfip.edu.co>
2019-11-10 16:20:33 -0500

    Template.

--- congreso/conexion.php
+++ congreso/conexion.php
@@ -1,8 +1,8 @@
 <?php
     $s="localhost";
-    $u="User_Covaite";
-    $p="eD@241FLO?4R3";
-    $b="Covaite";
+    $u="mateus_root";
+    $p="12345679";
+    $b="onlinecongress";
 
     $conex = new mysqli($s, $u, $p, $b) or die('No se pudo conectar a la Base de Datos');
     mysqli_set_charset($conex, "utf8");
